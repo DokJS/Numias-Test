39586ed23ccd3839daaa11316e11ce663631021d
Omar Mbengue <ombengue@sqli.com>
2023-03-19 16:27:35 +0000

    Feat : Handle login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,56 +1,51 @@
 import React, { useState } from 'react'
+import { useAppDispatch } from './redux/hook'
+import { login } from './redux/slices/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
-	const [username, setUsername] = useState('bob')
-	const [password, setPassword] = useState('abc123')
+    const [username, setUsername] = useState('bob')
+    const [password, setPassword] = useState('abc123')
+    const navigate = useNavigate()
+    const dispatch = useAppDispatch()
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log('submitting')
+        dispatch(login({ username, password }))
+        setTimeout(() => {
+            navigate('/wallet')
+        }, 2000)
+    }
 
-	const handleSubmit = async (e: React.FormEvent) => {
-		e.preventDefault()
-
-		const res = await fetch('/token', {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json',
-			},
-			body: JSON.stringify({
-				username,
-				password,
-			}),
-		})
-
-		const json = await res.json()
-		console.log('auth token', json.token)
-	}
-
-	return (
-		<div>
-			<h1>My Wallet</h1>
-			<h2>Login</h2>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Username:
-					<input
-						type="text"
-						value={username}
-						onChange={(
-							e: React.ChangeEvent<HTMLInputElement>
-						) => setUsername(e.target.value)}
-					/>
-				</label>
-				<label>
-					Password
-					<input
-						type="password"
-						value={password}
-						onChange={(
-							e: React.ChangeEvent<HTMLInputElement>
-						) => setPassword(e.target.value)}
-					/>
-				</label>
-				<button type="submit">Send</button>
-			</form>
-		</div>
-	)
+    return (
+        <div>
+            <h1>My Wallet</h1>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setUsername(e.target.value)
+                        }
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setPassword(e.target.value)
+                        }
+                    />
+                </label>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    )
 }
 
 export default App
--- client/src/redux/slices/authSlice.ts
+++ client/src/redux/slices/authSlice.ts
@@ -1,21 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { API_HOST, apiEndpoint, getUrl } from '../../util/api'
 interface InitialState {
     isAuth: boolean
     isLoading: boolean
-    error: string
+    isError: boolean
     accessToken: string
 }
 const initialState: InitialState = {
     isAuth: false,
     isLoading: false,
-    error: '',
+    isError: false,
     accessToken: '',
 }
+type User = {
+    username: string
+    password: string
+}
+export const login = createAsyncThunk(
+    'auth/login',
+    async (data: User, thunkAPI) => {
+        const url = getUrl(API_HOST, apiEndpoint.token)
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        const result = await response.json()
+        return result
+    }
+)
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(login.pending, (state) => {
+            state.isLoading = true
+        })
+        builder.addCase(login.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isAuth = true
+            state.accessToken = action.payload.token
+        })
+        builder.addCase(login.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+        })
+    },
 })
 
 export default authSlice.reducer
