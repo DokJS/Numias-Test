38de43fb78372004de1f6bd0831a8ceb0328a3e0
Omar Mbengue <ombengue@sqli.com>
2023-03-19 17:37:29 +0000

    Feat : Handle fetch wallet

--- client/src/Components/Wallet.tsx
+++ client/src/Components/Wallet.tsx
@@ -1,7 +1,16 @@
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../redux/hook'
+import { fetchWallet } from '../redux/slices/walletSlice'
 export default function Wallet() {
-	return (
-		<div>
-			<h1>Wallet</h1>
-		</div>
-	)
+    const dispatch = useAppDispatch()
+    const token = useAppSelector((state) => state.auth.accessToken)
+    console.log(token)
+    useEffect(() => {
+        dispatch(fetchWallet(token)) //For fetch wallet data
+    }, [])
+    return (
+        <div>
+            <h1>Wallet</h1>
+        </div>
+    )
 }
--- client/src/redux/slices/walletSlice.ts
+++ client/src/redux/slices/walletSlice.ts
@@ -1,21 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { getUrl, apiEndpoint, API_HOST } from '../../util/api'
 interface InitialState {
     wallet: []
     isLoading: boolean
-    error: string
+    error: boolean
 }
 
 const initialState: InitialState = {
     wallet: [],
     isLoading: false,
-    error: '',
+    error: false,
 }
-
+export const fetchWallet = createAsyncThunk(
+    'wallet/fetchWallet',
+    async (token: string, thunkAPI) => {
+        const url = getUrl(API_HOST, apiEndpoint.assets)
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: ` ${token}`,
+                },
+            })
+            const data = await response.json()
+            return data
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error)
+        }
+    }
+)
 const walletSlice = createSlice({
     name: 'wallet',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchWallet.pending, (state) => {
+            state.isLoading = true
+        })
+        builder.addCase(fetchWallet.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.wallet = action.payload
+        })
+        builder.addCase(fetchWallet.rejected, (state) => {
+            state.isLoading = false
+            state.error = true
+        })
+    },
 })
 
 export default walletSlice.reducer
